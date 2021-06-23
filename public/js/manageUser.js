a51d30fd0acc4957c32d90f8bf8729c3e80fa362
Nathan Capiaux <capiauxnathan@gmail.com>
2021-06-23 16:57:36 +0200

    create student + almost teacher

---
+++ public/js/manageUser.js
@@ -0,0 +1,113 @@
+function handleDeleteUser(event){
+    const userId = $(this).val();
+
+    console.log(userId);
+    $.ajax({
+        url: "/manage/user/" + userId,
+        method : "DELETE",
+        success: function (data) {
+            $('#userTable').DataTable().row(event.target.closest("tr")).remove().draw();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            $("#alert-container").append(createAlert("danger", "Erreur lors de la suppression de l'utiliseur", `Erreur !`));
+        }
+    });
+}
+
+function handleAddUserSucces(data){
+    $('#userTable').DataTable().row.add([
+        data.id,
+        data.email,
+        data.firstname,
+        data.lastname,
+        data.role,
+        "<button type=\"button\" class=\"deleteuser btn btn-danger\" value=\""+data.id+ "\"><i class=\"bi bi-trash\"></i></button>"
+    ]).draw();
+
+    $("#alert-container").append(createAlert("success", `L'utilisateur ${firstname} ${lastname} a bien été ajouté !`, `Succès !`));
+}
+
+function generatePassword(){
+    return Math.random().toString(36).slice(-10);
+}
+
+$(function () {
+
+    let userTable = $('#userTable').DataTable({
+        columnDefs: [
+            { width: '5%', targets: 0 },
+        ],
+    });
+
+    $(document).on('click','.deleteUser',handleDeleteUser);
+
+    $('.dispModal').click(() => {
+        $(".emailInput").val("");
+        $(".passwordInput").val(generatePassword());
+        $(".firstnameInput").val("");
+        $(".lastnameInput").val("");
+    });
+
+    $("#addStudentBtn").click(() => {
+        const email = $("#studentEmail").val().trim();
+        const password = $("#studentPassword").val().trim();
+        const firstname = $("#studentFirstName").val().trim();
+        const lastname = $("#studentLastName").val().trim();
+        const promo =$("#inputPromo").val();
+
+        if(email !== "" && password !== "" && firstname !== "" && lastname !== ""){
+            $.post({
+                url: '/manage/user',
+                data: {
+                    'email' : email,
+                    'password' : password,
+                    'firstname' : firstname,
+                    'lastname' : lastname,
+                    'promo' : promo,
+                    'roles' : [
+                        "ROLE_USER",
+                        "ROLE_STUDENT"
+                    ]
+                },
+                success : handleAddUserSucces,
+                error: (data) => {
+                    $("#alert-container").append(createAlert("danger", "Erreur lors de la création de l'utilisateur !" + data.message, `Erreur !`));
+                }
+            })
+        }
+    });
+
+
+    $("#addTeacherBtn").click(() => {
+        const email = $("#teacherEmail").val().trim();
+        const password = $("#teacherPassword").val().trim();
+        const firstname = $("#teacherFirstName").val().trim();
+        const lastname = $("#teacherLastName").val().trim();
+        const subjects = $("#inputSubject").val();
+        const promos = $("#inputPromoTeacher").val();
+
+
+        if(email !== "" && password !== "" && firstname !== "" && lastname !== ""){
+            $.post({
+                url: '/manage/user',
+                data: {
+                    'email' : email,
+                    'password' : password,
+                    'firstname' : firstname,
+                    'lastname' : lastname,
+                    'subjects' : subjects,
+                    'promos' : promos,
+                    'roles' : [
+                        "ROLE_USER",
+                        "ROLE_TEACHER"
+                    ]
+                },
+                success : handleAddUserSucces,
+                error: (data) => {
+                    $("#alert-container").append(createAlert("danger", "Erreur lors de la création de l'utilisateur !" + data.message, `Erreur !`));
+                }
+            })
+        }
+    });
+
+});
--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\PromoRepository;
+use App\Repository\SubjectRepository;
+use App\Repository\UserRepository;
 use App\Service\PromoService;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,14 +17,19 @@ class AdminController extends AbstractController
 {
 
     private $promoRepository;
+    private $userRepository;
+    private $subjectRepository;
 
-    public function __construct(PromoRepository $promoRepository)
+    public function __construct(PromoRepository $promoRepository, UserRepository $userRepository,
+                                SubjectRepository $subjectRepository)
     {
         $this->promoRepository = $promoRepository;
+        $this->userRepository = $userRepository;
+        $this->subjectRepository = $subjectRepository;
     }
 
     /**
-     * @Route("/manage/promo", name="admin", methods={"GET"})
+     * @Route("/manage/promo", name="admin_promo", methods={"GET"})
      */
     public function index(): Response
     {
@@ -31,6 +39,30 @@ class AdminController extends AbstractController
             'promos' => $promos,
         ]);
     }
+
+    /**
+     * @Route("/manage/user", name="admin_user", methods={"GET"})
+     */
+    public function manageUser(): Response
+    {
+        $users = $this->userRepository->findAll();
+        $promos = $this->promoRepository->findAll();
+        $subjects = $this->subjectRepository->findAll();
+
+        return $this->render('admin/manageUser.html.twig', [
+            'users' => $users,
+            'promos' => $promos,
+            "subjects" => $subjects
+        ]);
+    }
+
+    /**
+     * @Route("/manage/user/{id}", name="remove_user", methods={"DELETE"})
+     */
+    public function removeUser(UserService $userService, int $id): JsonResponse{
+        return $userService->removeUser($id);
+    }
+
 
     /**
      * @Route("/manage/promo/{id}", name="remove_promo", methods={"DELETE"})
@@ -45,6 +77,13 @@ class AdminController extends AbstractController
     public function addPromo(PromoService $promoService,Request $request): JsonResponse{
         $promoName = $request->get('promoName');
         return $promoService->addPromo($promoName);
+    }
+
+    /**
+     * @Route("/manage/user", name="add_user", methods={"POST"})
+     */
+    public function addUser(UserService $userService,Request $request): JsonResponse{
+        return $userService->addUser($request);
     }
 
 }
